ccd5020eeb6ce0e9ebbe700ae4f62617493cff29
Scott Reinhardt <sreinhardt@sagepath.com>
2018-04-21 00:43:21 -0400

    Legacy optimizer first pass

---
+++ Legacy Optimizer/Deck.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Legacy_Optimizer
+{
+    public class Decks
+    {
+        public List<Deck> PossibleDecks { get; set; }
+    }
+    public class Deck
+    {
+        public IEnumerable<Constraint> Constraints { get; set; }
+
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    public class Constraint
+    {
+        public string Name { get; set; }
+        public int Count { get; set; }
+    }
+
+    public class Collection
+    {
+        public IEnumerable<Constraint> Haves { get; set; }
+    }
+}
--- Legacy Optimizer/Program.cs
+++ Legacy Optimizer/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Legacy_Optimizer
 {
@@ -10,6 +12,56 @@ namespace Legacy_Optimizer
     {
         static void Main(string[] args)
         {
+            var collectionString = File.ReadAllText("collection.json");
+            var collection = JsonConvert.DeserializeObject<Collection>(collectionString);
+            var deckString = File.ReadAllText("decks.json");
+            var decks = JsonConvert.DeserializeObject<Decks>(deckString);
+            var solutions = PossibleDecks(decks, collection, new LinkedList<Decks>(), 0, new LinkedList<Deck>());
+
+            File.WriteAllLines("decks.txt", solutions.Where(s => s.PossibleDecks.Count >= 8).Select(s => string.Join(", ", s.PossibleDecks)));
+        }
+
+        static IEnumerable<Decks> PossibleDecks(Decks decks, Collection collection, LinkedList<Decks> solutions, int place, LinkedList<Deck> chain)
+        {
+            if (place == decks.PossibleDecks.Count)
+            {
+                solutions.AddLast(new Decks()
+                {
+                    PossibleDecks = chain.ToList()
+                });
+
+                return solutions;
+            }
+
+            if(CanAdd(decks.PossibleDecks[place], chain, collection))
+            {
+                chain.AddLast(decks.PossibleDecks[place]);
+                PossibleDecks(decks, collection, solutions, place + 1, chain);
+                chain.RemoveLast();
+            }
+
+            PossibleDecks(decks, collection, solutions, place + 1, chain);
+
+
+            return solutions;
+        }
+
+        static bool CanAdd(Deck deck, LinkedList<Deck> chain, Collection collection)
+        {
+            foreach (var constraint in deck.Constraints)
+            {
+                var total =
+                    chain.SelectMany(d => d.Constraints).Where(c => c.Name == constraint.Name).Sum(c => c.Count) +
+                    constraint.Count;
+
+                var have = collection.Haves.FirstOrDefault(c => c.Name == constraint.Name);
+                if (have == null || have.Count < total)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
